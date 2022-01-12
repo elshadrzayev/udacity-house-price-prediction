cd233b5f69c05f1d1653d96f40fc07bf652bad92
EC2 Default User <ec2-user@ip-172-31-77-238.ec2.internal>
2022-01-12 09:29:57 +0000

    push file

--- run_kubernetes.sh
+++ run_kubernetes.sh
@@ -5,14 +5,23 @@
 # Step 1:
 # This is your Docker ID/path
 # dockerpath=<>
+dockerpath=elshadrzayev/app
 
 # Step 2
 # Run the Docker Hub container with kubernetes
+docker login
+kubectl run house-prediction\
+    --image=$dockerpath\
+    --port=80 --labels app=app
+
 
 
 # Step 3:
 # List kubernetes pods
 
+kubectl get pods
+
 # Step 4:
 # Forward the container port to a host
+kubectl port-forward house-prediction 8080:80
 
